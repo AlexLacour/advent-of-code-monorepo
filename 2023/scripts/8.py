e36a14f1cdada54ad70da2f417e276f97f19fa1f
AlexLacour <alexandre.lacour@xxii.fr>
2023-12-08 11:47:17 +0100

    day8

--- 2023/scripts/8.py
+++ 2023/scripts/8.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-import numpy as np
+import math
 
 from aoc_utils import read_input
 
@@ -21,7 +19,9 @@ def parse_raw_nodes(node_str_list: list[str]) -> dict:
 input_nodes = parse_raw_nodes(raw_input_nodes)
 
 
-def get_number_of_steps_from_given_start_to_a_z(start: str, directions: str, nodes: dict) -> int:
+def get_number_of_steps_from_given_start_to_a_z(
+    start: str, directions: str, nodes: dict
+) -> int:
     current_node = start
     n_steps = 0
     while not current_node.endswith("Z"):
@@ -35,51 +35,22 @@ def get_number_of_steps_from_given_start_to_a_z(start: str, directions: str, nod
 def get_number_of_steps_from_all_a_to_all_z(
     directions: str, nodes: dict[str, dict]
 ) -> int:
-
-    node_id_to_node_name = [node_name for node_name in nodes]
-
-    nodes_np_graph = np.zeros((2, len(nodes)), dtype=np.int16)  # 2 <=> L/R
-    for node_name, left_right_nodes in nodes.items():
-        left_node, right_node = left_right_nodes.values()
-        nodes_np_graph[0][
-            node_id_to_node_name.index(node_name)
-        ] = node_id_to_node_name.index(left_node)
-        nodes_np_graph[1][
-            node_id_to_node_name.index(node_name)
-        ] = node_id_to_node_name.index(right_node)
-
-    # Starting and ending nodes
-    starting_node_ids = np.array(
-        [
-            node_id_to_node_name.index(node_name)
-            for node_name in nodes
-            if node_name.endswith("A")
-        ]
-    )
-    ending_node_ids = np.array(
-        [
-            node_id_to_node_name.index(node_name)
-            for node_name in nodes
-            if node_name.endswith("Z")
-        ]
-    )
-
-    directions = directions.replace("L", "0")
-    directions = directions.replace("R", "1")
+    # Starting nodes
+    starting_nodes = [node_name for node_name in nodes if node_name.endswith("A")]
 
     n_steps_collection = []
-    for starting_node_id in starting_node_ids:
-        n_steps = 0
-        current_node = starting_node_id
-        while current_node not in ending_node_ids:
-            direction_to_take = int(directions[n_steps % len(directions)])
+    for starting_node in starting_nodes:
+        n_steps_collection.append(
+            get_number_of_steps_from_given_start_to_a_z(
+                starting_node, directions, nodes
+            )
+        )
 
-            current_node = nodes_np_graph[direction_to_take][current_node]
-            n_steps += 1
-        n_steps_collection.append(n_steps)
-
-    return n_steps_collection
+    return math.lcm(*n_steps_collection)
 
 
-# print("P1", get_number_of_steps_from_aaa_to_zzz(input_directions, input_nodes))
+print(
+    "P1",
+    get_number_of_steps_from_given_start_to_a_z("AAA", input_directions, input_nodes),
+)
 print("P2", get_number_of_steps_from_all_a_to_all_z(input_directions, input_nodes))
