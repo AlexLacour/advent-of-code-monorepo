b7a84266b0fe1ee1736a9ab951cb2763fdc5ceda
AlexLacour <alexandre.lacour@xxii.fr>
2023-12-14 13:11:49 +0100

    day14

--- 2023/scripts/14.py
+++ 2023/scripts/14.py
@@ -1,5 +1,6 @@
 import copy
 import hashlib
+from collections import Counter, defaultdict
 from typing import Optional
 
 import numpy as np
@@ -9,54 +10,62 @@ from aoc_utils import read_input
 
 input_rocks_map = read_input(as_type=list, to_numpy=True)
 
-DIRECTIONS = {"N": (-1, 0), "S": (1, 0), "E": (0, 1), "W": (0, -1)}
 
-
-def move_rocks(static_rocks: set, movable_rocks: dict, direction: str) -> set[tuple]:
-    new_static_rocks = set()
-
-    while any(val is not None for val in movable_rocks.values()):
-        for rock_id, rock in movable_rocks.items():
-            if rock is None:
-                continue
-            potential_new_position = rock + DIRECTIONS[direction]
-            if (
-                not 0 <= potential_new_position[0] < len(input_rocks_map)
-                or not 0 <= potential_new_position[1] < len(input_rocks_map[0])
-                or tuple(potential_new_position) in static_rocks
-                or tuple(potential_new_position) in new_static_rocks
-            ):
-                new_static_rocks.add(tuple(rock))
-                movable_rocks[rock_id] = None
-            else:
-                movable_rocks[rock_id] = potential_new_position
-    return new_static_rocks
-
-
-def get_load(initial_rocks_map: np.ndarray, directions: list[str], n_cycles: int = 1) -> int:
-    height, width = initial_rocks_map.shape
-
-    initial_movable_rocks = {
-        rock_id: np.array((x, y))
-        for rock_id, (x, y) in enumerate(zip(*np.where(initial_rocks_map == "O")))
-    }
-
-    static_rocks = set(
-        [(x, y) for x, y in zip(*np.where(initial_rocks_map == "#"))]
+def move_rocks(rocks_map: np.ndarray):
+    rocks_position = sorted(
+        [(line, col) for line, col in zip(*np.where(rocks_map == "O"))]
     )
 
-    movable_rocks = copy.deepcopy(initial_movable_rocks)
-    for i in tqdm(range(n_cycles)):
-        for direction in directions:
+    for rock in rocks_position:
+        line, col = rock
+        above = rocks_map[:line, col][::-1]
 
-            movable_rocks_set = move_rocks(static_rocks, copy.deepcopy(movable_rocks), direction)
-            movable_rocks = {rock_id: np.array(rock_pos) for rock_id, rock_pos in enumerate(movable_rocks_set)}
+        n_moves = 0
+        for c in above:
+            if c != ".":
+                break
+            n_moves += 1
 
-    result = sum(height - (rock[0]) for rock in movable_rocks.values())
+        rocks_map[line, col] = "."  # start moving
+        rocks_map[line - n_moves, col] = "O"
+    return rocks_map
 
-    return result
+
+def get_load(rocks_map: np.ndarray):
+    height, _ = rocks_map.shape
+    return sum(
+        height - pos[0]
+        for pos in [(line, col) for line, col in zip(*np.where(rocks_map == "O"))]
+    )
 
 
+def run_cycle(rocks_map: np.ndarray) -> np.ndarray:
+    for _ in ["N", "W", "S", "E"]:  # counter clockwise
+        rocks_map = np.rot90(move_rocks(rocks_map), axes=(1, 0))
+    return rocks_map
 
-print("P1", get_load(input_rocks_map, directions=["N"]))
-# print("P2", get_load(input_rocks_map, directions=["N", "W", "S", "E"], n_cycles=1000000000))
+
+print("P1", get_load(move_rocks(input_rocks_map)))
+
+n_cycles = 1000000000
+maps_history = []
+rocks_map = input_rocks_map
+period = 0
+offset = 0
+loads = []
+for cycle_id in range(n_cycles):
+    rocks_map = run_cycle(rocks_map)
+    loads.append(get_load(rocks_map))
+
+    rocks_map_str = "".join(str(el) for el in np.nditer(rocks_map))
+    if not rocks_map_str in maps_history:
+        maps_history.append(rocks_map_str)
+    else:
+        last_seen_id = maps_history.index(rocks_map_str)
+        period = cycle_id - last_seen_id
+        offset = cycle_id - period  # time before the period starts
+        loads = loads[-period - 1 : -1]  # only the non repeating part
+        break
+
+load_index = (n_cycles - offset) % period
+print("P2", loads[load_index - 1])
