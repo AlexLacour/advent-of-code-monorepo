aa3d5202dff69c1a28c963fe22da88ce25431964
AlexLacour <alexandre.lacour@xxii.fr>
2024-08-13 11:17:43 +0200

    day 8

---
+++ 2016/scripts/8.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from aoc_utils import read_input
+
+input_screen_instructions = read_input(as_type=lambda instr: instr.split())
+
+
+screen = np.zeros((6, 50))
+
+for instruction in input_screen_instructions:
+    match instruction[0]:
+        case "rect":
+            width, height = map(int, instruction[-1].split("x"))
+            screen[:height, :width] = 1.0
+        case "rotate":
+            match instruction[1]:
+                case "row":
+                    row_id = int(instruction[2].split("=")[-1])
+                    shift = int(instruction[-1])
+
+                    screen[row_id] = np.roll(screen[row_id], shift)
+                case "column":
+                    col_id = int(instruction[2].split("=")[-1])
+                    shift = int(instruction[-1])
+                    screen[:, col_id] = np.roll(screen[:, col_id], shift)
+
+print(f"{np.sum(screen, dtype=int)=}")
+
+for row in screen:
+    row_str = "".join(["\u2588" if val else " " for val in row])
+    print(row_str)
