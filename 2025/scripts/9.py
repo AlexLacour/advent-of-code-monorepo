0d737d88501ea3a297b28e990e6b7d483dcd6824
AlexLacour <alexandre.lacour3@hotmail.fr>
2025-12-10 12:08:58 +0100

    day9

--- 2025/scripts/9.py
+++ 2025/scripts/9.py
@@ -1,17 +1,34 @@
 import numpy as np
 from aoc_utils import read_input
+import shapely
 
 
 def parse_coordinate(yx_str: str) -> tuple[int, ...]:
     return tuple(map(int, yx_str.split(",")))
 
 
+def rect_area(x1: int, y1: int, x2: int, y2: int) -> int:
+    return (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+
+
 input_red_tiles = read_input(as_type=parse_coordinate)
+
+full_area = shapely.Polygon(input_red_tiles)
 
 max_area = 0
 for pid, p1 in enumerate(input_red_tiles):
     for p2 in input_red_tiles[pid:]:
-        area = (abs(p2[0] - p1[0]) + 1) * (abs(p2[1] - p1[1]) + 1)
+        # area = rect_area(*p1, *p2)
 
-        max_area = max(area, max_area)
+        x1, y1 = p1
+        x2, y2 = p2
+        xmin, xmax = min(x1, x2), max(x1, x2)
+        ymin, ymax = min(y1, y2), max(y1, y2)
+
+        rectangle = shapely.Polygon(
+            [(xmin, ymin), (xmin, ymax), (xmax, ymax), (xmax, ymin)]
+        )
+
+        if rectangle.within(full_area):
+            max_area = max(rect_area(*p1, *p2), max_area)
 print(max_area)
